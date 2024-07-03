5c45b42f932f6da3739a45c45513a407a17ca3f0
Jorgecas71 <jorgecas71@gmail.com>
2024-07-04 00:46:29 +0200

    added changes staged

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -3,10 +3,18 @@
 </script>
 
 <template>
+    <div class="greetings">
+        <h1 class="green">{{ msg }}</h1>
+        <h3>
+            Mapa de incendios 
+
+        </h3>
+    </div>
     <div class="card" style="width: 18rem;">
         <img src="../assets/images/mapafuegosok.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-            <p class="card-text">Previsualiza el mapa de información de los incendios que se encuentran en España en estos momentos.</p>
+            <p class="card-text">Previsualiza el mapa de información de los incendios que se encuentran en España en
+                estos momentos.</p>
         </div>
     </div>
 </template>
