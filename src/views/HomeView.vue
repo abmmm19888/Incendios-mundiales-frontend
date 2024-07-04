091ee9bcd46c68ed57243bec27009e83134469c6
Jorgecas71 <jorgecas71@gmail.com>
2024-07-05 00:27:49 +0200

    added changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Navbar from '../../src/components/Navbar.vue';
-import Cards from '../../src/components/Cards.vue';
+import Cards from '@/components/cards.vue';
 import Footer from '../../src/components/Footer.vue';
 </script>
 
