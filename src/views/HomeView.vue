a4e64851e748d43dd3bad0b2d55ad5289a0931dd
Aitor Garcia Fernandez <aitorgarciafernandezcv@gmail.com>
2024-07-04 10:11:57 +0200

    website fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-
-<Navbar></Navbar>
-
+  <Navbar></Navbar>
 </template>
