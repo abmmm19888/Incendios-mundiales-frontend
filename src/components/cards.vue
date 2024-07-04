12986e79fafa4511ac81864c3fad6e08249760e0
Jorgecas71 <jorgecas71@gmail.com>
2024-07-04 09:17:44 +0200

    added navbar

---
+++ src/components/cards.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+<template>
+    <div class="card text-bg-dark">
+        <img src="../assets/images/fueg1.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+
+            <p class="card-text">"Cada acción cuenta para prevenir incendios.¡Cuidemos nuestros bosques!"</p>
+        </div>
+    </div>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="../assets/images/mapafuegosok.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Maps</h5>
+            <p class="card-text">Previsualiza el mapa de información de los incendios que se encuentran en España en
+                estos
+                momentos.</p>
+        </div>
+    </div>
+</template>
+<styles lang="scss"></styles>
